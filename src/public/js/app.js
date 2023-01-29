5a1e7b8f8805b0a9e385329b07b757a3f7dbf8aa
June <46692136+wnsjunedud@users.noreply.github.com>
2023-01-29 12:39:07 -0500

    chat complete

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,26 @@
-//frontend
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅")
+function handleOpen() {
+  console.log("Connected to Server ✅");
+}
+
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
 });
 
-socket.addEventListener("message" , (message) => {
-    console.log("New message: ", message.data);
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
 });
 
-socket.addEventListener("close" , () => {
-    console.log("Disconnected from Server ❌");
-});
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);//10000-wait 10sec
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
 import http from "http";
 import WebSocket from "ws";
-import express from "express";
-import { Console } from "console";
-//express - set the views and render
-//rest - websocket
+import express from "express"; //express - set the views and render
+
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app); // i have access
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-//http, websocket on the same port
-//app.listen(3000,handleListen);
+
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
 
 wss.on("connection", (socket) => {
-    console.log("Connected to Browser ✅")
-    //not a method in server or something
-    //method in socket
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-    socket.on("message", (message) => {
-        console.log(message.toString('utf8'));
-    });
-    socket.send("hello~!");
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
 });
 
-server.listen(3000,handleListen);
+server.listen(3000, handleListen);
