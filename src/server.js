878267e5238165bc2a58d0c015874010a3b193a5
June <46692136+wnsjunedud@users.noreply.github.com>
2023-02-06 12:31:11 -0500

    User Count
    Count user inside the room

--- src/server.js
+++ src/server.js
@@ -29,28 +29,38 @@ function publicRooms() {
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+  //set (2.10-2:00
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) =>{
     console.log(`Socket Event: ${event}`);
   });
+
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname);
+    socket.to(roomName).emit("welcome", socket.nickname,countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());
   });
+
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => 
-    socket.to(room).emit("bye", socket.nickname));
+    socket.to(room).emit("bye", socket.nickname,countRoom(room)-1));
   });
+
   socket.on("disconnect", () => {
     wsServer.sockets.emit("room_change", publicRooms());
   });
+
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   });
+  
   socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
